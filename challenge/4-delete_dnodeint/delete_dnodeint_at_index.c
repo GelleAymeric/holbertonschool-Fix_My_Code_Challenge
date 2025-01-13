b479fc3464191c17b5eac5dc5804d4f5fffc7713
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-13 09:58:49 +0100

    Refactor delete_dnodeint_at_index for improved readability and correctness; simplify node deletion logic

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,8 +1,9 @@
+
 #include "lists.h"
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list 
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
  * @head: A pointer to the first element of a list
  * @index: The index of the node to delete
@@ -43,13 +44,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = *head;
-		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-		{
-			tmp->next->prev = tmp->prev;
-		}
-		free(tmp);
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
 		*head = saved_head;
 	}
 	return (1);

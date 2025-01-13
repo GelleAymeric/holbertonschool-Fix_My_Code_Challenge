d308f11f1daa0c861d10789869173c4723d73ce7
Aymeric154 <aymeric.gelle@outlook.fr>
2025-01-13 10:14:01 +0100

    Refactor FizzBuzz script to improve input validation; ensure a single numeric argument is provided

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
 import sys
 
 
@@ -26,14 +29,8 @@ def fizzbuzz(n):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) <= 1:
         print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    if not sys.argv[1].isdigit():
-        print("Invalid number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
